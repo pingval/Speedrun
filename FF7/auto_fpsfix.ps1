b68b90e6651989ac192bf3a14d495a93f227916e
pingval <40169989+pingval@users.noreply.github.com>
2020-11-17 18:05:26 +0900

    Add files via upload

--- FF7/auto_fpsfix.ps1
+++ FF7/auto_fpsfix.ps1
@@ -7,29 +7,28 @@ $FPSFIX_path = 'C:\salon\game\research\ff7\FPSFIX\FPSFIX.exe'
 $sleep_seconds = 5
 
 while ($true) {
-    $FF7_Launcher_alive = Get-Process $FF7_Launcher_process -ErrorAction SilentlyContinue
-    if ($FF7_Launcher_alive) {
-        try{
-            $p = Get-Process $FF7_process -ErrorAction SilentlyContinue
-            $p.WaitForExit()
-        } catch {
-            Write-Host "No $FF7_process Process"
-            $FPSFIX_alive = Get-Process $FPSFIX_process -ErrorAction SilentlyContinue
-            If (!$FPSFIX_alive) {
-                Write-Host "No $FPSFIX_process Process; Start $FPSFIX_path"
-                Invoke-Item $FPSFIX_path
-            }
-        } finally{
-        }
-        try {
-            $p = Get-Process $FPSFIX_process -ErrorAction SilentlyContinue
-            $p.WaitForExit()
-        } catch {
-            Write-Host "No $FPSFIX_process Process"
-        }
-
-    } else {
-        Write-Host "No $FF7_Launcher_process Process; Sleep $sleep_seconds seconds..."
-        Sleep $sleep_seconds
+  $FF7_Launcher_alive = Get-Process $FF7_Launcher_process -ErrorAction SilentlyContinue
+  if ($FF7_Launcher_alive) {
+    try{
+      $p = Get-Process $FF7_process -ErrorAction SilentlyContinue
+      $p.WaitForExit()
+    } catch {
+      Write-Host "No $FF7_process Process"
+      $FPSFIX_alive = Get-Process $FPSFIX_process -ErrorAction SilentlyContinue
+      If (!$FPSFIX_alive) {
+        Write-Host "No $FPSFIX_process Process; Start $FPSFIX_path"
+        Invoke-Item $FPSFIX_path
+      }
+    } finally {
+      try {
+        $p = Get-Process $FPSFIX_process -ErrorAction SilentlyContinue
+        $p.WaitForExit()
+      } catch {
+        Write-Host "No $FPSFIX_process Process"
+      }
     }
+  } else {
+    Write-Host "No $FF7_Launcher_process Process; Sleep $sleep_seconds seconds..."
+    Sleep $sleep_seconds
+  }
 }
